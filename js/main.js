bf5f91c396fa526ba19c47cefc4f07376b1bf3fa
iaras <s.arai.unicom@gmail.com>
2024-04-08 17:27:18 +0900

    header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready( function () {
   $('#table').DataTable();
+
+  $(".openbtn").click(function () {//ボタンがクリックされたら
+    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+      $("#nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+  });
+  
+  $("#nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+      $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+      $("#nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+  });
+
 } );
