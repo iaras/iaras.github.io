776183ea20efc2a3a04dcb3ef6b28affcf9550ae
iaras <s.arai.unicom@gmail.com>
2024-04-16 19:30:08 +0900

    data

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(document).ready( function () {
     "language" : {"url": "https://cdn.datatables.net/plug-ins/2.0.3/i18n/ja.json"},
     ajax: {url: "../data/ekimemo.json"},
     columns: [
-      {data: 'No.'},
+      {data: 'No'},
       {data: '名前'},
       {data: 'タイプ'},
       {data: '属性'},
