a4d9c1c39a954f123f0dc48f55ddb336d82a8994
iaras <s.arai.unicom@gmail.com>
2024-04-16 19:01:51 +0900

    data

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 $(document).ready( function () {
   $('#table').DataTable();
+  $('#ekimemo').DataTable({
+    ajax: {url: "data/ekimemo.json"},
+    columns: [
+      {data: 'No.'},
+      {data: '名前.'},
+      {data: 'タイプ.'},
+      {data: '属性'},
+    ]
+  });
 } );
