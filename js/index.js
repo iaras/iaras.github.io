0ee7a5ff2c972b7f2888fee5939d116556acff92
iaras <s.arai.unicom@gmail.com>
2024-04-16 19:25:12 +0900

    js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $(document).ready( function () {
   $('#table').DataTable();
   $('#ekimemo').DataTable({
+    "language" : {"url": "https://cdn.datatables.net/plug-ins/2.0.3/i18n/ja.json"},
     ajax: {url: "../data/ekimemo.json"},
     columns: [
       {data: 'No.'},
-      {data: '名前.'},
-      {data: 'タイプ.'},
+      {data: '名前'},
+      {data: 'タイプ'},
       {data: '属性'},
     ]
   });
