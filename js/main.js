7f8961845dca2eb4468013773f384612169301d4
iaras <s.arai.unicom@gmail.com>
2024-04-16 18:17:27 +0900

    page

--- js/main.js
+++ js/main.js
@@ -9,4 +9,10 @@ $(document).ready( function () {
       $("#nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
   });
 
+  // メニューの差し込み
+  append_string = '<li><a href="../index.html">TOP</a></li>'
+  append_string += '<li><a href="pages/tenki.html">天気予報</a></li>'
+  append_string += '<li><a href="pages/jma.html">気象庁</a></li>'
+  $("#nav-list ul").append(append_string)
 } );
+
